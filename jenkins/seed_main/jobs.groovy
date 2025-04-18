817b7fec3a06fef42948a0434577f52a25d12c64
ALAM <sk.maifujalam@gmail.com>
2025-04-18 10:32:15 +0530

    lite k8s

--- jenkins/seed_main/jobs.groovy
+++ jenkins/seed_main/jobs.groovy
@@ -29,3 +29,33 @@ pipelineJob('cd-pipeline') {
         }
     }
 }
+pipelineJob('evicted-pod-cleaner.build') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/maifujalam/k8s_aws_lite.git')
+                    }
+                    branch('main')
+                }
+                scriptPath('app-build-pipeline/evicted-pod-cleaner/Jenkinsfile') // Path to your pipeline script
+            }
+        }
+    }
+}
+pipelineJob('jnlp-agent.build') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/maifujalam/k8s_aws_lite.git')
+                    }
+                    branch('main')
+                }
+                scriptPath('app-build-pipeline/jnlp-agent/Jenkinsfile') // Path to your pipeline script
+            }
+        }
+    }
+}

974190bf4c88980056e8e219a549d72d12c546ed
ALAM <sk.maifujalam@gmail.com>
2025-04-19 12:03:50 +0530

    lite k8s

--- shellscripts/secrets/git_secret.sh
+++ shellscripts/secrets/git_secret.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euo pipefail
+
+GIT_USER=Jenkins
+
 if [ -z "${GIT_TOKEN:-}" ];then
   echo "GIT_TOKEN is not set"
   exit 1
 fi
 kubectl -n jenkins delete secret git-secret
-kubectl -n jenkins create secret generic git-secret --from-literal=GIT_TOKEN=${GIT_TOKEN}
+kubectl -n jenkins create secret generic git-secret --from-literal=GIT_USER=${GIT_USER} --from-literal=GIT_TOKEN=${GIT_TOKEN}

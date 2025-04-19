1f4fae714fe92c9cf313357b8a8866dd1be16cdd
ALAM <sk.maifujalam@gmail.com>
2025-04-19 09:33:51 +0530

    lite k8s

--- shellscripts/secrets/aws_secret.sh
+++ shellscripts/secrets/aws_secret.sh
@@ -3,7 +3,7 @@
 set -eou pipefail
 
 if [ -z "${AWS_ACCESS_KEY_ID:-}" ] || [ -z "${AWS_SECRET_ACCESS_KEY:-}" ];then
-  echo ❌ AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY is not set"
+  echo "❌ AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY is not set"
   exit 1
 fi
 

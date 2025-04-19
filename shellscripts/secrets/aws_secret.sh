0108fbde72d268792f2992d64d8cebc8c693e033
ALAM <sk.maifujalam@gmail.com>
2025-04-19 09:32:10 +0530

    lite k8s

--- shellscripts/secrets/aws_secret.sh
+++ shellscripts/secrets/aws_secret.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+set -eou pipefail
+
+if [ -z "${AWS_ACCESS_KEY_ID}:-" || -z "${AWS_SECRET_ACCESS_KEY}:-" ];then
+  echo "AWS AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY is not set"
+  exit 1
+fi
+
+
 kubectl -n jenkins delete secret aws-secret
 kubectl -n jenkins create secret generic aws-secret --from-literal=AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
 --from-literal=AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
--- shellscripts/secrets/git_secret.sh
+++ shellscripts/secrets/git_secret.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 if [ -z "${GIT_TOKEN:-}" ];then
   echo "GIT_TOKEN is not set"
+  exit 1
 fi
 kubectl -n jenkins delete secret git-secret
 kubectl -n jenkins create secret generic git-secret --from-literal=GIT_TOKEN=${GIT_TOKEN}

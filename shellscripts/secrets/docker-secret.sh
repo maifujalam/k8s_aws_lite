619ed1a9e3a9b9810c27058bef51dc6ccb09967e
ALAM <sk.maifujalam@gmail.com>
2025-04-19 09:27:06 +0530

    lite k8s

--- shellscripts/secrets/docker-secret.sh
+++ shellscripts/secrets/docker-secret.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 set -euo pipefail  # Run: bash kaniko-secret.sh
+# e: exit immediately if any command exit with non zero.
+# u: Consider unset value as an error and exit
+# -o pipefail:  If there is any intermittent pipe fails in the script.The entire script fails.
 
 # Ensure DOCKER_PASSWORD is set
-if [ -z "${DOCKER_PASSWORD:-}" ]; then
+if [ -z "${DOCKER_PASSWORD:-}" ]; then  # -z: means if empty value
   echo "❌ DOCKER_PASSWORD is not set. Please export it before running this script."
   exit 1
 fi
--- shellscripts/secrets/git_secret.sh
+++ shellscripts/secrets/git_secret.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -euo pipefail
+if [ -z "${GIT_TOKEN:-}" ];then
+  echo "GIT_TOKEN is not set"
+fi
 kubectl -n jenkins delete secret git-secret
 kubectl -n jenkins create secret generic git-secret --from-literal=GIT_TOKEN=${GIT_TOKEN}

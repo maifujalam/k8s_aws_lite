49bcb8c21a73d4078574c10b5646ec423bcd341e
ALAM <sk.maifujalam@gmail.com>
2025-05-04 22:03:36 +0530

    my commit

--- shellscripts/secrets/aws_secret.sh
+++ shellscripts/secrets/aws_secret.sh
@@ -8,11 +8,11 @@ if [ -z "${AWS_ACCESS_KEY_ID:-}" ] || [ -z "${AWS_SECRET_ACCESS_KEY:-}" ];then
 fi
 
 
-kubectl -n jenkins delete secret aws-secret
+kubectl -n jenkins delete secret aws-secret --ignore-not-found=
 kubectl -n jenkins create secret generic aws-secret --from-literal=AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
 --from-literal=AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 
-kubectl -n cert-manager delete secret aws-credentials-secret
+kubectl -n cert-manager delete secret aws-credentials-secret --ignore-not-found=
 kubectl -n cert-manager create secret generic aws-credentials-secret \
 --from-literal=aws_access_key_id=${AWS_ACCESS_KEY_ID} --from-literal=aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
 
--- shellscripts/secrets/git_secret.sh
+++ shellscripts/secrets/git_secret.sh
@@ -7,5 +7,5 @@ if [ -z "${GIT_TOKEN:-}" ];then
   echo "GIT_TOKEN is not set"
   exit 1
 fi
-kubectl -n jenkins delete secret git-secret
+kubectl -n jenkins delete secret git-secret --ignore-not-found
 kubectl -n jenkins create secret generic git-secret --from-literal=GIT_USER=${GIT_USER} --from-literal=GIT_TOKEN=${GIT_TOKEN}

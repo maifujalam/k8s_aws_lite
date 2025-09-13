379b70d783a5fe58bb8e38204dc1468eaff1666a
skmaifujalam <sk.maifujalam@gmail.com>
2025-09-13 10:26:33 +0530

    expand root partition Workers

--- jenkins-agent-raw/jenkins-agent/install_kubectl.sh
+++ jenkins-agent-raw/jenkins-agent/install_kubectl.sh
@@ -3,5 +3,5 @@ set -e
 
 curl -LO "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
+mv kubectl /usr/local/bin/
 kubectl version --client

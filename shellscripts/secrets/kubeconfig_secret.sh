b5ae8c159f46a5b98583eecbdcad7eb392038585
skmaifujalam <sk.maifujalam@gmail.com>
2025-09-21 17:52:07 +0530

    expand root partition Workers

--- shellscripts/secrets/kubeconfig_secret.sh
+++ shellscripts/secrets/kubeconfig_secret.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-kubectl -m jenkins create secret generic kubeconfig-secret --from-file=kubeconfig=$HOME/.kube/config
+kubectl -n jenkins delete secret kubeconfig-secret --ignore-not-found
+kubectl -n jenkins create secret generic kubeconfig-secret --from-file=KUBECONFIG=$HOME/.kube/config
 echo "kubeconfig-secret created in kube-system namespace"

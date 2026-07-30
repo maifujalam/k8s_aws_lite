fc7b1ea0e735ecb7f53d9d65f8bec89f9879803f
skmaifujalam <sk.maifujalam@gmail.com>
2026-07-30 11:37:04 +0530

    alibaba cloud

--- terraform/lambdas/cloud-eventbridge-lambda/main.tf
+++ terraform/lambdas/cloud-eventbridge-lambda/main.tf
@@ -23,11 +23,21 @@ resource "aws_cloudwatch_event_target" "lambda_target_stop" {
   arn       = data.aws_lambda_function.get_lambda_stop_vm.arn
 }
 
-# # Permission so EventBridge can invoke Lambda
-# resource "aws_lambda_permission" "allow_eventbridge" {
-#   statement_id  = "AllowExecutionFromEventBridge"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.rtrigger_labda.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.daily_trigger.arn
-# }
+# Permission so EventBridge can invoke Start Lambda
+resource "aws_lambda_permission" "allow_eventbridge_start" {
+  statement_id  = "AllowExecutionFromEventBridgeStart"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.get_lambda_start_vm.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily_trigger_start.arn
+}
+
+
+# Permission so EventBridge can invoke Stop Lambda
+resource "aws_lambda_permission" "allow_eventbridge_stop" {
+  statement_id  = "AllowExecutionFromEventBridgeStop"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.get_lambda_stop_vm.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily_trigger_stop.arn
+}

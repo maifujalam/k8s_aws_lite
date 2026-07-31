e67f17747b6c774d5e217fdc8c56baacc4221589
skmaifujalam <sk.maifujalam@gmail.com>
2026-07-31 09:25:11 +0530

    alibaba cloud

--- shellscripts/nat-forward.sh
+++ shellscripts/nat-forward.sh
@@ -1,20 +1,24 @@
 sudo apt -y update
 sudp apt -y upgrade
-sudo apt install -y iptables iptables-persistent
+sudo apt install -y iptables iptables-persistent netfilter-persistent
+
+# Forward trrafic of ipv4 between interface.
 cat <<EOF | sudo tee /etc/sysctl.d/nat.conf
   net.ipv4.ip_forward                 = 1
 EOF
 # Apply sysctl params without reboot
 sudo sysctl --system
 
-sudo iptables -t nat -A POSTROUTING -o enX0 -j MASQUERADE   # WARNING: replace enX0 with your public interface name, e.g. eth0, ens5, etc.
+# Any packet leaving via interface ens5 (your public NIC) will have its source IP rewritten to the public IP of that interface.
+sudo iptables -t nat -A POSTROUTING -o ens5 -j MASQUERADE   # WARNING: replace enX0 with your public interface name, e.g. eth0, ens5, etc.
 
-# make IP table persistent
+# Flush any forwarding rule present.
 sudo /sbin/iptables -F FORWARD
 
-sudo netfilter-persistent save
+# Reload to make the changes effect.
 sudo netfilter-persistent reload
 
+# Lists NAT table rules with line numbers.Confirms that your POSTROUTING MASQUERADE rule is active
 sudo iptables -t nat -L --line-number
 
 # Disable source destination  check for NAT instance: https://docs.aws.amazon.com/vpc/latest/userguide/work-with-nat-instances.html#EIP_Disable_SrcDestCheck

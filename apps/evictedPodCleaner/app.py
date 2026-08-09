aeacafa65af1955c8e61370775b3fa1a26922c5e
skmaifujalam <sk.maifujalam@gmail.com>
2026-08-09 11:05:41 +0530

    alibaba cloud

--- apps/evictedPodCleaner/app.py
+++ apps/evictedPodCleaner/app.py
@@ -23,25 +23,30 @@ if __name__ == '__main__':
         pods=v1.list_pod_for_all_namespaces(watch=False)
         count=0
         for pod in pods.items:
-            print("Checking Pod:"+pod.metadata.name + " Namespace: " + pod.metadata.namespace + " Status: " + pod.status.phase)
+            #print("Checking Pod:"+pod.metadata.name + " Namespace: " + pod.metadata.namespace + " Status: " + pod.status.phase)
+            if pod.status.init_container_statuses:
+                print("Got Init container.")
+                for init_containers in pod.status.init_container_statuses:
+                    print("Checking InitContainer: "+init_containers.name+", of Pod: "+pod.metadata.name)
+                    if init_containers.state.terminated:
+                        if init_containers.state.terminated.exit_code !=0:
+                            print("InitContainer: " +init_containers.name+" Exit code non Zero: "+str(init_containers.state.terminated.exit_code))
+                            print("Deleting whole pod to restart it.")
+                            v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
+                            print("Deleted Succeeded Pod " + pod.metadata.name)
+                            sleep(300)
+
             if pod.status.phase != 'Running':
                 print("Non Running Pod"+pod.metadata.name+ " | "+pod.metadata.namespace+" | "+pod.status.phase)
                 try:
                     if pod.status.reason or pod.status.phase:
-                        # if pod.status.reason == "Evicted":
-                        #     print("Deleting Evicted Pod "+pod.metadata.name)
-                        #     v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
-                        #     print("Deleted Evicted Pod "+pod.metadata.name)
-                        # if pod.status.phase == "Succeeded":
-                        #     print("Deleting Succeeded Pod "+pod.metadata.name)
-                        #     v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
-                        #     print("Deleted Succeeded Pod "+pod.metadata.name)
-                        # Check initContainerStatuses for CrashLoopBackOff
-                        if pod.status.init_container_statuses:
-                            for init_cs in status.init_container_statuses:
-                                if init_cs.state and init_cs.state.waiting and init_cs.state.waiting.reason == "CrashLoopBackOff":
-                                    print(f"Deleting pod {pod_name} (initContainer in CrashLoopBackOff)")
-                                    #v1.delete_namespaced_pod(name=pod_name, namespace=namespace)
-                                    break
+                        if pod.status.reason == "Evicted":
+                            print("Deleting Evicted Pod "+pod.metadata.name)
+                            v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
+                            print("Deleted Evicted Pod "+pod.metadata.name)
+                        if pod.status.phase == "Succeeded":
+                            print("Deleting Succeeded Pod "+pod.metadata.name)
+                            v1.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
+                            print("Deleted Succeeded Pod "+pod.metadata.name)
                 except:
                     print("Failed to get pod status")
